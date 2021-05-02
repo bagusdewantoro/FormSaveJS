9386ce7b16d229a61ad045b601e0de1a199b9422
bagusdewantoro <bagusdewantoro86@gmail.com>
2021-05-03 05:13:35 +0700

    highest profit

--- jobs/views.py
+++ jobs/views.py
@@ -5,9 +5,19 @@ from django.urls import reverse
 
 def costView(request):
     lastResult = Cost.objects.all().last()
-    highestCost = Cost.objects.all().order_by('-costModel').first()
-    highestIncome = Cost.objects.all().order_by('-incomeModel').first()
+    costList = Cost.objects.all().order_by('-costModel')
+    incomeList = Cost.objects.all().order_by('-incomeModel')
 
+    # generate highest profit:
+    profitList = []
+    for datas in Cost.objects.all():
+        x1 = datas.incomeModel
+        y1 = datas.costModel
+        z1 = datas.dateModel
+        profitList.append([x1-y1, z1])
+    orderedProfitList = sorted(profitList, reverse=True)
+
+    # main form statements:
     if request.method == 'POST':
         form = CostForm(request.POST)
         if form.is_valid():
@@ -19,7 +29,7 @@ def costView(request):
             return redirect(reverse('cost'))
     else:
         form = CostForm()
-    context = {'form':form, 'lastData':lastResult, 'topCost':highestCost,
-                'topIncome': highestIncome}
+    context = {'form':form, 'lastData':lastResult, 'listOfCost':costList,
+                'listOfIncome': incomeList, 'listOfProfit': orderedProfitList}
 
     return render(request, 'jobs/cost.html', context)
